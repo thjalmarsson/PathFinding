0ebf46b77eba79df257f93e775dad4425ee492ae
Tobias <tob.hjalmarsson@gmail.com>
2020-09-11 18:07:29 +0200

    Working pathfinder
    The pathfinder is fully working but the code will need refactoring

--- main.py
+++ main.py
@@ -27,25 +27,35 @@ maze1 = [["X","X","X","X","X","X","X","X","X","X"],
 		["X","O","O","O","O","O","O","F","O","X"],
 		["X","X","X","X","X","X","X","X","X","X"]]
 
+
+maze2 = [["X","X","X","X","X","X","X","X","X","X"],
+		["X","O","O","X","O","O","O","O","O","X"],
+		["X","O","S","X","O","O","O","O","O","X"],
+		["X","O","O","X","O","X","X","X","O","X"],
+		["X","O","O","X","O","X","O","O","O","X"],
+		["X","O","O","X","O","X","O","O","O","X"],
+		["X","O","O","O","O","X","O","F","O","X"],
+		["X","X","X","X","X","X","X","X","X","X"]]
+
 """
 Functions that we are going to need
 1. valid_node() Check if the next step is a valid step i.e not a wall or not a previously visited node [Done]
 2. move_node() that steps in a direction (going to call them up, down, left ,right), going to use a string to 
 	represent these, Up = "U", Left = "L", Down = "D", Right = "R"
 """
-
+# checks if the path is valid
 def valid_node(maze,moves, row, col):
 	new_row = row
 	new_col = col
 	for move in moves:
 		if move == "U":
-			new_row = row - 1
+			new_row = new_row - 1
 		elif move == "D":
-			new_row = row + 1
+			new_row = new_row + 1
 		elif move == "L":
-			new_col = col - 1
+			new_col = new_col - 1
 		elif move == "R":
-			new_col = col + 1
+			new_col = new_col + 1
 
 	if maze[new_row][new_col] == "X":
 		return False
@@ -55,29 +65,36 @@ def valid_node(maze,moves, row, col):
 		return False
 	else:
 		return True
-
+# Returns true if its the "goal node"
 def finish_node(maze, row, col):
 	if maze[row][col] == "F":
+		current_row, current_col = move_node(maze, current_path, starting_row, starting_col, "0")
 		return True
 	else:
 		return False
 
-def move_node(maze, moves, row, col):
+# Takes a list of moves then sets those moves to +
+def move_node(maze, moves, row, col, symbol):
 	new_row = row
 	new_col = col
 	for move in moves:
 		if move == "U":
-			new_row = row - 1
-			maze[new_row][new_col] = "+"
-		elif move == "D":
-			new_row = row + 1
-			maze[new_row][new_col] = "+"
-		elif move == "L":
-			new_col = col - 1
-			maze[new_row][new_col] = "+"
+			new_row = new_row - 1
+			if maze[new_row][new_col] != "F":
+				maze[new_row][new_col] = symbol
 		elif move == "R":
-			new_col = col + 1
-			maze[new_row][new_col] = "+"
+			new_col = new_col + 1
+			if maze[new_row][new_col] != "F":
+				maze[new_row][new_col] = symbol
+		elif move == "L":
+			new_col = new_col - 1
+			if maze[new_row][new_col] != "F":
+				maze[new_row][new_col] = symbol
+		elif move == "D":
+			new_row = new_row + 1
+			if maze[new_row][new_col] != "F":
+				maze[new_row][new_col] = symbol
+	return new_row, new_col
 
 def print_maze(maze):
 	for row in maze:
@@ -98,31 +115,21 @@ q = queue.Queue()
 for direction in directions:
 	q.put(direction)
 
+
+starting_row = 2
+starting_col = 2
 current_row = 2
 current_col = 2
-
+current_path = ""
 # Here I am implementing the main loop to search through the grid
-"""
-while not finish_node(maze1, current_row, current_col):
-	for direction in directions:
-		my_queue.put(direction)
+while not finish_node(maze2, current_row, current_col):
+	current_path = q.get()
+	if valid_node(maze2, current_path, starting_row, starting_col):
+		current_row, current_col = move_node(maze2, current_path, starting_row, starting_col, "+")
+		print("row: " + str(current_row) + ", col: " + str(current_col))
+		for direction in directions:
+			q.put(current_path + direction)
 
-"""
-
-
-
-"""
-for direction in directions:
-	my_queue.put(direction)
-print_queue(my_queue)
-"""
-
-"""
-
-new_test_row, new_test_col = move_node(maze1, "L", old_test_row, old_test_col)
-print(maze1[new_test_row][new_test_col])
-"""
-old_test_row = 2
-old_test_col = 2
-new_test_row, new_test_col = move_node(maze1, "L", old_test_row, old_test_col)
-print_maze(maze1)
+print_maze(maze2)
+print("Finished at row: " + str(current_row) + " and col: " + str(current_col) + " with path: " + current_path)
+#Problemet nu är att den markerar målnoden som + och därav inte känner av att den ska bryta
